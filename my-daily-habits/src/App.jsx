a583cd2fa2a76c03e0324f97f3b2de95c8971ae8
Amanda <amandaufrr23@gmail.com>
2026-03-11 20:44:48 -0400

    criação de novos componentes adicionando props para o arquivo de jsx nomeusuario, mensagem usando condicionais

--- my-daily-habits/src/App.jsx
+++ my-daily-habits/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Footer from './components/Footer'
+import Header from './components/Header'
+import BemVindo from './components/BemVindo'
+
+// Componente cabecalho 
 
 function App() {
   return ( 
   <div>
-    <h1>My Daily Habits</h1>
-    <p>Gerencie seus hábitos diários de forma simples e visual.</p>
+    <Header titulo={"My Daily Habits"} descricao={"Gerencie seus hábitos diários de forma simples e visual."}/>
+    <BemVindo nomeUsuario={"Amanda Barbosa"} totalHabitos={2}/>
     <Footer />
   </div>
   )
---
+++ my-daily-habits/src/components/BemVindo.jsx
@@ -0,0 +1,17 @@
+const BemVindo = ({nomeUsuario, totalHabitos}) => {
+    const nomeFormatado = nomeUsuario.toUpperCase()
+
+    const mensagem = totalHabitos > 0 
+    ? `Você tem ${totalHabitos} hábitos(s) cadastrados.`
+    : `Nenhum hábito cadastrado ainda. Que tal começar?`
+
+    return (
+        <div>
+            <h2>Olá, {nomeFormatado}</h2>
+            <p>{mensagem}</p>
+        </div>
+    )
+}
+
+export default BemVindo
+
